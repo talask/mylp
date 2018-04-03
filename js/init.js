cc74c64d3f45eb0d02b5bb2967d5b7531265ea76
talask <natasha169@ukr.net>
2018-04-03 22:46:43 +0300

    change index.html style init.js

--- js/init.js
+++ js/init.js
@@ -7,6 +7,6 @@ $( function() {
 $('.reviews-carousel').slick({
     dots: true,
     prewArrow: '<div class="slick-perv slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-left"></i></div></div>',
-    nextArrow: '<div class="slick-perv slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-right"></i></div></div>',
+    nextArrow: '<div class="slick-next slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-right"></i></div></div>',
 });
   });
