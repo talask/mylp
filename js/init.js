8c6944882d802caf62f7fcbe600c0fd60ca9f1be
talask <natasha169@ukr.net>
2018-04-03 16:16:18 +0300

    + каорусель и контакты только по видео без дз

--- js/init.js
+++ js/init.js
@@ -3,4 +3,10 @@ $( function() {
     $( "#tabs" ).tabs();
     // Init tabs
     $( "#tabs-portfolio" ).tabs();
-  } );
+    //init slick carousel
+$('.reviews-carousel').slick({
+    dots: true,
+    prewArrow: '<div class="slick-perv slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-left"></i></div></div>',
+    nextArrow: '<div class="slick-perv slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-right"></i></div></div>',
+});
+  });
