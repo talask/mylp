1f656a44e3346a1927965a7ed1bcbca42dbd2675
talask <natasha169@ukr.net>
2018-04-08 01:05:25 +0300

    поправила карусель

--- js/init.js
+++ js/init.js
@@ -6,8 +6,8 @@ $( function() {
     //init slick carousel
 $('.reviews-carousel').slick({
     dots: true,
-    prewArrow: '<div class="slick-perv slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-left"></i></div></div>',
-    nextArrow: '<div class="slick-next slick-arrow"><dif class="carousel-button"><i class="fa-fa-chewron-right"></i></div></div>',
+    prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+    nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
 });
   });
   var map;
